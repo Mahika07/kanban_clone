8c554f1a2e4c38bdc0cb705d67a5232361621b66
Mahika <mahikasharma0007@gmail.com>
2023-06-20 00:33:00 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Rightsidebar from './components/Rightsidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Rightsidebar />
+    </div>
+  );
+}
+
+export default App;
